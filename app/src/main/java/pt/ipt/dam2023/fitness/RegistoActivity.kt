66f576df130051414a9dd88cf9b1bd776c1bbc62
Dianaleite01 <aluno23392@ipt.pt>
2024-01-21 00:53:29 +0000

    teste (ignorar este commit)

--- app/src/main/java/pt/ipt/dam2023/fitness/RegistoActivity.kt
+++ app/src/main/java/pt/ipt/dam2023/fitness/RegistoActivity.kt
@@ -1,46 +1,13 @@
 package pt.ipt.dam2023.fitness
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import pt.ipt.dam2023.fitness.ui.theme.FitnessTheme
 
-class RegistoActivity : ComponentActivity() {
+class RegistoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FitnessTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_registo)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitnessTheme {
-        Greeting("Android")
     }
 }
