47626c5c3cbe6f8bc7a02ed427f86869f2ce5e87
Dianaleite01 <aluno23392@ipt.pt>
2024-01-23 00:17:28 +0000

    menuprincipal
    base do menu principal concluida- botoes para respetivas paginas a funcionar

--- app/src/main/java/pt/ipt/dam2023/fitness/MenuPrincipalActivity.kt
+++ app/src/main/java/pt/ipt/dam2023/fitness/MenuPrincipalActivity.kt
@@ -1,11 +1,60 @@
 package pt.ipt.dam2023.fitness
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
 class MenuPrincipalActivity : AppCompatActivity() {
+
+    //DECLARACAO DAS VARIAVEIS
+    private lateinit var qrcodeCard: CardView
+    private lateinit var galeriaCard: CardView
+    private lateinit var aulasCard: CardView
+    private lateinit var corridaCard: CardView
+    private lateinit var calendarioCard: CardView
+    private lateinit var logoutCard: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menuprincipal)
+
+        //INICIALIZA AS REFERENCIAS PARA OS CARDVIEW USANDO FINDVIEWBYID
+        qrcodeCard = findViewById(R.id.qrcodeCard)
+        galeriaCard = findViewById(R.id.galeriaCard)
+        aulasCard = findViewById(R.id.aulasCard)
+        corridaCard = findViewById(R.id.corridaCard)
+        calendarioCard = findViewById(R.id.calendarioCard)
+        logoutCard = findViewById(R.id.logoutCard)
+
+
+        //CONFIGURACAO DO ONCLICKLISTENER PARA CADA CARDVIEW
+
+        qrcodeCard.setOnClickListener {
+            val intent = Intent(this@MenuPrincipalActivity, QrCodeActivity::class.java)
+            startActivity(intent)
+        }
+
+        galeriaCard.setOnClickListener {
+            val intent = Intent(this@MenuPrincipalActivity, GaleriaActivity::class.java)
+            startActivity(intent)
+        }
+
+        aulasCard.setOnClickListener {
+            val intent = Intent(this@MenuPrincipalActivity, AulasActivity::class.java)
+            startActivity(intent)
+        }
+
+        corridaCard.setOnClickListener {
+            val intent = Intent(this@MenuPrincipalActivity, CorridaActivity::class.java)
+            startActivity(intent)
+        }
+
+        calendarioCard.setOnClickListener {
+            val intent = Intent(this@MenuPrincipalActivity, CalendarioActivity::class.java)
+            startActivity(intent)
+        }
+
+        //FALTA O LOGOUT (SAIR DA CONTA)
     }
 }
