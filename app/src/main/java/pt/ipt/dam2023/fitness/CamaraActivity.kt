9c8177cf4fa5e842dc558d227b298a3c519314de
PabloHatesTacos <aluno23395@ipt.pt>
2024-01-28 22:06:03 +0000

    fim

--- app/src/main/java/pt/ipt/dam2023/fitness/CamaraActivity.kt
+++ app/src/main/java/pt/ipt/dam2023/fitness/CamaraActivity.kt
@@ -1,26 +1,38 @@
 package pt.ipt.dam2023.fitness
 
+import android.Manifest
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Base64
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import android.Manifest
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.ByteArrayOutputStream
 
+@Suppress("DEPRECATION")
 class CamaraActivity : AppCompatActivity() {
 
-    lateinit var imageView: ImageView
+    private lateinit var imageView: ImageView
     lateinit var button: Button
-    val REQUEST_IMAGE_CAPTURE = 100
-    val MY_CAMERA_PERMISSION_CODE = 101
+    private lateinit var adicionarbutton: Button
+    private lateinit var buttonBackToMenu: Button
+    private var imageBitmap: Bitmap? = null
+    private val requestimagecapture = 100
+    private val mycamerapermissioncode = 101
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,16 +40,41 @@ class CamaraActivity : AppCompatActivity() {
 
         requestCameraPermission()
 
+        val btnVoltarMenuPrincipal: Button = findViewById(R.id.btnVoltarMenuPrincipal)
+        btnVoltarMenuPrincipal.setOnClickListener {
+            val intent = Intent(this@CamaraActivity, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+        }
+
         imageView = findViewById(R.id.camara)
         button = findViewById(R.id.tirarfotoButton)
+        adicionarbutton = findViewById(R.id.adicionarFotoButton)
+        buttonBackToMenu = findViewById(R.id.btnVoltarMenuPrincipal)
+
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
 
         button.setOnClickListener{
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
             try{
-                startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE)
+                startActivityForResult(takePictureIntent,requestimagecapture)
             }catch (e: ActivityNotFoundException){
                 Toast.makeText(this, "Error:" + e.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonBackToMenu.setOnClickListener(View.OnClickListener { v: View? ->
+            val intent = Intent(
+                this@CamaraActivity, MenuPrincipalActivity::class.java)
+            startActivity(intent)
+            finish()
+        })
+
+        adicionarbutton.setOnClickListener{
+            if (imageBitmap != null) {
+                showOptionsDialog()
+            } else {
+                Toast.makeText(this, "Tire uma fotografia antes de adicionar", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -51,28 +88,86 @@ class CamaraActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(Manifest.permission.CAMERA),
-                MY_CAMERA_PERMISSION_CODE
+                mycamerapermissioncode
             )
         }
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            val imageBitmap = data?.extras?.get("data") as Bitmap
+        if(requestCode == requestimagecapture && resultCode == RESULT_OK){
+            imageBitmap = data?.extras?.get("data") as Bitmap
             imageView.setImageBitmap(imageBitmap)
-        }
-        else{
+        } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
-    fun tirarFoto(view: View) {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-        } catch (e: ActivityNotFoundException) {
-            Toast.makeText(this, "Error: ${e.localizedMessage}", Toast.LENGTH_SHORT).show()
+    private fun showOptionsDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Adicionar Foto")
+        builder.setMessage("Deseja adicionar esta foto ao perfil?")
+        builder.setPositiveButton("Adicionar") { _, _ ->
+            uploadFoto(imageBitmap!!, sharedPreferences.getString("userId", "") ?: "")
         }
+        builder.setNegativeButton("Descartar") { _, _ ->
+            // Limpar a variável imageBitmap se o usuário decidir descartar
+            imageBitmap = null
+        }
+        builder.create().show()
     }
 
+    private fun saveBase64ImageToSharedPreferences(base64Image: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("userImage", base64Image)
+        editor.apply()
+    }
+    private fun bitmapToBase64(bitmap: Bitmap): String {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        val byteArray = byteArrayOutputStream.toByteArray()
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
+    }
+
+    private fun uploadFoto(bitmap: Bitmap, userId: String) {
+        val base64Image = bitmapToBase64(bitmap)
+        if (userId.isEmpty()) {
+            Toast.makeText(this@CamaraActivity, "ID do user não encontrado.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        saveBase64ImageToSharedPreferences(base64Image)
+        val user = User(id = userId, email = "", nome = "", password = "", ftperfil = base64Image, peso = "", altura = "", codGym = 0, imc = "", dieta = "", admin = false)
+        val newUserRequest = UserRequest(user)
+        val token = "GonDi"
+        val authHeader = "Bearer $token"
+        val call = ApiService().service().createUser(newUserRequest, authHeader)
+        call.enqueue(object : Callback<UserRequest> {
+            override fun onResponse(call: Call<UserRequest>, response: Response<UserRequest>) {
+                if (response.isSuccessful) {
+                    val userIdFromResponse = response.body()?.user?.id
+                    if (userIdFromResponse != null) {
+                        saveUserIdToSharedPreferences(userIdFromResponse)
+                        Toast.makeText(this@CamaraActivity, "Foto de perfil atualizada com sucesso!", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // Lidar com o caso em que o ID do user é nulo
+                        Toast.makeText(this@CamaraActivity, "ID do usuário nulo na resposta.", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // Lidar com a resposta não bem-sucedida
+                    Toast.makeText(this@CamaraActivity, "Erro ao atualizar a foto do perfil. Tente novamente mais tarde.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<UserRequest>, t: Throwable) {
+                // Lidar com a falha na comunicação com o servidor
+                Toast.makeText(this@CamaraActivity, "Erro de conexão. Verifique sua conexão com a internet e tente novamente.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun saveUserIdToSharedPreferences(userId: String?) {
+        val editor = sharedPreferences.edit()
+        editor.putString("userId", userId)
+        editor.apply()
+    }
 }
--- app/src/main/java/pt/ipt/dam2023/fitness/MenuPrincipalActivity.kt
+++ app/src/main/java/pt/ipt/dam2023/fitness/MenuPrincipalActivity.kt
@@ -1,7 +1,14 @@
 package pt.ipt.dam2023.fitness
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -15,10 +22,15 @@ class MenuPrincipalActivity : AppCompatActivity() {
     private lateinit var corridaCard: CardView
     private lateinit var calendarioCard: CardView
     private lateinit var logoutCard: CardView
+    private lateinit var txtNome: TextView
+    private lateinit var fotoPerfil: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menuprincipal)
+
+        txtNome = findViewById(R.id.txtNome)
+        fotoPerfil = findViewById(R.id.fotoPefil)
 
         //INICIALIZA AS REFERENCIAS PARA OS CARDVIEW USANDO FINDVIEWBYID
         qrcodeCard = findViewById(R.id.qrcodeCard)
@@ -28,6 +40,24 @@ class MenuPrincipalActivity : AppCompatActivity() {
         calendarioCard = findViewById(R.id.calendarioCard)
         logoutCard = findViewById(R.id.logoutCard)
 
+        val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        val userId = sharedPreferences.getString("iduser", "")
+        Log.d("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB", "BBBBBBBBBB: $userId")
+
+        // Obtém a string base64 da SharedPreferences
+        val base64Image = getBase64ImageFromSharedPreferences(userId ?: "")
+
+        if (base64Image != null) {
+            // Converte a string de base64 para Bitmap
+            val bitmapImage = base64ToBitmap(base64Image)
+            // Define o Bitmap na ImageView
+            fotoPerfil.setImageBitmap(bitmapImage)
+            Log.d("AAAAAAAAAAAAAAA", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+
+        } else {
+            Log.e("MenuPrincipalActivity", "Base64 da imagem não encontrado.")
+            fotoPerfil.setImageResource(R.drawable.ic_launcher_foreground)
+        }
 
         //CONFIGURACAO DO ONCLICKLISTENER PARA CADA CARDVIEW
 
@@ -47,7 +77,7 @@ class MenuPrincipalActivity : AppCompatActivity() {
         }
 
         corridaCard.setOnClickListener {
-            val intent = Intent(this@MenuPrincipalActivity, SobreActivity::class.java)
+            val intent = Intent(this@MenuPrincipalActivity, CorridaActivity::class.java)
             startActivity(intent)
         }
 
@@ -66,6 +96,21 @@ class MenuPrincipalActivity : AppCompatActivity() {
         super.onBackPressed()
     }
 
+    private fun getBase64ImageFromSharedPreferences(iduser: String): String? {
+        val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        return sharedPreferences.getString("userImage_$iduser", null)
+    }
+
+    fun base64ToBitmap(base64String: String): Bitmap? {
+        try {
+            val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
+            return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
     private fun showLogoutConfirmationDialog() {
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Confirmação")
@@ -78,10 +123,12 @@ class MenuPrincipalActivity : AppCompatActivity() {
         }
 
         builder.setNegativeButton("Não") { dialog, which ->
-            // Não faz nada, usuário optou por não sair
+            // Não faz nada, user optou por não sair
         }
 
         val dialog = builder.create()
         dialog.show()
     }
+
+
 }
